7ce3670811fc35d8326d123fa131ab17b5a2577f
Gunar C. Gessner <gunar@gunargessner.com>
2018-09-05 13:21:37 -0300

    unitize lifetime opacity and add random opacitiy to particles

--- sketch.js
+++ sketch.js
@@ -2,13 +2,14 @@ const sx = 0
 const sy = 0
 const sw = 400
 const sh = 400
-const S = 3 // size
-const N_PARTICLES = 300
+const S = 2 // size
 const SPACING = 2
 const CX = sw/2
 const CY = sh/2
 
 const LIFETIME = 100
+const N_PARTICLES = sw*0.5234532
+const MAX_OPACITIY = 256
 
 // Golden Ratio
 const PHI = (1 + Math.sqrt(5)) / 2
@@ -43,7 +44,7 @@ function setup() {
     return sqrt(k-.5)/sqrt(n-(b+1)/2)
   }
   const rand = x => random(-x,x)
-  const n = sw*0.4234532
+  const n = N_PARTICLES
   const b = round(ALPHA*sqrt(n)) // number of boundary points
   particles = arr(n)
     .map((_, k) => ({
@@ -52,6 +53,7 @@ function setup() {
       theta: 2*PI*k/PHI^2,
       rotation: rand(PI/400),
       t: random(0, LIFETIME),
+      opacity: random(.5,1)
     }))
 }
 
@@ -72,16 +74,16 @@ function draw() {
     const { r, theta } = p
     const x = CX*r*cos(theta)
     const y = CY*r*sin(theta)
-    const transparency = f => {
-      const maxOpacity = 200
+    const lifetimeOpacity = () => {
       if (p.t < LIFETIME*1/4) {
-        return p.t*maxOpacity/(LIFETIME*1/4)
+        return p.t/(LIFETIME*1/4)
       } else if (p.t < LIFETIME*3/4) {
-        return maxOpacity
+        return 1
       }
-      return (LIFETIME-p.t)*maxOpacity/(LIFETIME*1/4)
+      return (LIFETIME-p.t)/(LIFETIME*1/4)
     }
-    rotating.fill(255,transparency(frameCount))
+    const initOpacity = () => limit(frameCount/50, 1)
+    rotating.fill(255,MAX_OPACITIY*lifetimeOpacity()*initOpacity()*p.opacity)
     rotating.ellipse(CX+x, CY+y, S, S)
   }
   copy(rotating, 0, 0, sw, sh, 0, 0, sw, sh)
